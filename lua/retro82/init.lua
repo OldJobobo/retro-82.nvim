b02709902737980e15605f3f9b0dff1c45fa56d9
J. S. Brown <jsbrown7@gmail.com>
2026-04-02 11:56:20 -0700

    Reapply lua LSP highlights after attach

--- lua/retro82/init.lua
+++ lua/retro82/init.lua
@@ -12,10 +12,46 @@ local modules = {
   "retro82.groups.integrations",
 }
 
+local lua_lsp_groups = {
+  "@lsp.type.namespace.lua",
+  "@lsp.type.parameter.lua",
+  "@lsp.type.property.lua",
+  "@lsp.type.variable.lua",
+  "@lsp.typemod.variable.defaultLibrary.lua",
+  "@lsp.typemod.function.defaultLibrary.lua",
+  "@lsp.typemod.method.defaultLibrary.lua",
+  "@lsp.typemod.property.defaultLibrary.lua",
+}
+
 local function apply_terminal_colors()
   for i, color in ipairs(palette.terminal) do
     vim.g["terminal_color_" .. (i - 1)] = color
   end
+end
+
+local function reapply_lua_lsp(groups)
+  for _, name in ipairs(lua_lsp_groups) do
+    local spec = groups[name]
+    if spec then
+      vim.api.nvim_set_hl(0, name, spec)
+    end
+  end
+end
+
+local function ensure_lua_lsp_autocmd(groups)
+  local augroup = vim.api.nvim_create_augroup("Retro82LuaLsp", { clear = true })
+
+  vim.api.nvim_create_autocmd("LspAttach", {
+    group = augroup,
+    callback = function(args)
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      if client and client.name == "lua_ls" then
+        vim.schedule(function()
+          reapply_lua_lsp(groups)
+        end)
+      end
+    end,
+  })
 end
 
 local function apply_options(groups)
@@ -56,6 +92,8 @@ function M.load()
 
   apply_options(groups)
   util.apply(groups)
+  ensure_lua_lsp_autocmd(groups)
+  reapply_lua_lsp(groups)
 end
 
 return M
